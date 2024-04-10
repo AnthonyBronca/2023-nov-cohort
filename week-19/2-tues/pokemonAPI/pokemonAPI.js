ff24e1843181cf3b724e46c0204baeac3b092fed
irelius <kihong711@hotmail.com>
2024-04-09 22:01:22 -0700

    updating main pokemonAPI file

--- week-19/2-tues/pokemonAPI/pokemonAPI.js
+++ week-19/2-tues/pokemonAPI/pokemonAPI.js
@@ -13,36 +13,33 @@ app.get('/', (req, res) => {
     // An "async" is NOT necessary for the fetch because of .then() chaining. The data is converted immediately afterwards
 app.get('/pokemon', (req, res) => {
     try {
-        fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        fetch('https://pokeapi.co/api/v2/pokemon/ditto')
             .then((res) => res.json())
             .then((pokemon) => res.json(pokemon))
-    } catch (error) {
-        console.error(e)
+    } catch (e) {
+        console.error(e.message)
     }
 })
-
 
 // Route to hit the API endpoint for any pokemon passed into the parameter
 // Includes a middleware: "noPokemon"
 // async is necessary here because no .then() chaining is used. The fetch promise needs to resolve first before moving onto the next steps
-app.get('/pokemon/:pokemon', noPokemon, async (req, res) => {
-    try {
-        const pokemon = req.params.pokemon
+app.get('/pokemon/:pokemonName', noPokemon, async (req, res) => {
+    try{
+        const pokemonName = req.params.pokemonName
+        const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const data = await pokemonData.json()
 
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-        const pokemonData = await response.json()
-        res.send(pokemonData)
-    } catch (e) {
-        res.send("Unknown error")
+        res.json(data)
+    } catch(e) {
+        console.error(e)
     }
 })
 
-
 // Catch for when users try to hit a URL that doesn't exist
 app.use((req, res) => {
-    res.send("Endpoint not found")
+    res.send("You found a non-existent endpoint")
 })
-
 
 const port = 3001
 app.listen(port, () => {
