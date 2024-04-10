8dd113d8dd4723579977a4b5560a7109d6f6e909
irelius <kihong711@hotmail.com>
2024-04-09 22:01:43 -0700

    updating middleware for pokemonAPI demonstration

--- week-19/2-tues/pokemonAPI/middleware/noPokemon.js
+++ week-19/2-tues/pokemonAPI/middleware/noPokemon.js
@@ -1,24 +1,20 @@
-const express = require('express')
-const app = express()
-
 // Middleware will check the pokemon name that gets passed in through the request body
-    // "response" will return a promise that is resolved that has a "status" property
+// "response" will return a promise that is resolved that has a "status" property
 // The try-catch block will run the "try" block first and when the error is thrown, the "catch" block will catch that error
-    // If the "status" code is something like 404, then you know the response wasn't good, and you can throw an error
+// If the "status" code is something like 404, then you know the response wasn't good, and you can throw an error
 const noPokemon = async function (req, res, next) {
     try {
-        const pokemon = req.params.pokemon
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        const pokemonName = req.params.pokemonName
+        const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
-        if (response.status !== 200) {
+        if (pokemonData.status !== 200) {
             throw new Error("Missingno found!")
         }
 
         return next()
-    } catch (error) {
-        res.send(error.message)
+    } catch (e) {
+        res.send(e.message)
     }
 }
-
 
 module.exports = noPokemon
