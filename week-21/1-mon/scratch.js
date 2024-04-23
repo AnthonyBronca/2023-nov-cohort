20a8e77fc66392402fd815a87a321069f5df1e41
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-04-23 19:03:23 -0400

    added notes for mon and tues

---
+++ week-21/1-mon/scratch.js
@@ -0,0 +1,17 @@
+
+
+
+
+// const people = ['sam', 'anthony', 'will', 'edward', 'gabe'];
+
+// // console.log(people.includes('will'));
+
+// const peopleObj = {
+//     1: "sam",
+//     2: "anthony",
+//     3: "will",
+//     4: "edward",
+//     5: "gabe"
+// };
+
+// console.log(peopleObj[3])
